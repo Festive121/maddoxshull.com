4d8c7d7f60ad9fd3614304db331e1d87b7acd7c8
Festive121 <79292822+Festive121@users.noreply.github.com>
2023-07-17 19:27:00 -0600

    good progress

--- Program.cs
+++ Program.cs
@@ -16,14 +16,11 @@ app.UseStaticFiles();
 
 app.MapGet("/", context =>
 {
-    if (context.Request.Cookies.TryGetValue("username", out _))
-    {
+    if (context.Request.Cookies.TryGetValue("username", out _)) {
         // If the username cookie exists, redirect the user to the /home page
         context.Response.Redirect("/home");
         return Task.CompletedTask;
-    }
-    else
-    {
+    } else {
         // If the username cookie doesn't exist, return the default index.html file
         var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "index.html");
         return context.Response.SendFileAsync(filePath);
@@ -39,47 +36,50 @@ app.MapGet("/collection", async (context) =>
     Console.WriteLine($"username: {username}");
 
     await using var connection = context.RequestServices.GetService<MySqlConnection>();
-    await using var checkCommand = connection.CreateCommand();
 
-    checkCommand.CommandText = "SELECT * FROM test.user_cards_view WHERE username = @username";
-    checkCommand.Parameters.AddWithValue("@username", username);
+    if (connection != null) {
+        await using var checkCommand = connection.CreateCommand();
 
-    using(MySqlDataReader reader = checkCommand.ExecuteReader())
-    {
-        while(await reader.ReadAsync())
-        {
-            // string? id = reader.GetString("cardid");
-            // string? src = reader.GetString("src");
-            await context.Response.WriteAsync($"<img id={reader.GetString("cardid")} src={reader.GetString("src")}></img>");
+        checkCommand.CommandText = "SELECT * FROM test.user_cards_view WHERE username = @username";
+        checkCommand.Parameters.AddWithValue("@username", username);
+
+        using(MySqlDataReader reader = checkCommand.ExecuteReader()) {
+            while(await reader.ReadAsync()) {
+                // string? id = reader.GetString("cardid");
+                // string? src = reader.GetString("src");
+                await context.Response.WriteAsync($"<img id={reader.GetString("cardid")} src={reader.GetString("src")}></img>");
+            }
         }
+    } else {
+        Console.WriteLine($"[{DateTime.Now}] Connection from [COLLECTION] is null");
     }
 });
 
-app.MapGet("/home", async (context) =>
+app.MapGet("/home", async (context) => 
 {
-    if (context.Request.Cookies.TryGetValue("username", out _))
-    {
-        await context.Response.WriteAsync(await File.ReadAllTextAsync("wwwroot/html/home.html"));
-
-        string username = context.Request.Cookies["username"];
+    if (context.Request.Cookies.TryGetValue("username", out _)) {
+        string? username = context.Request.Cookies["username"];
 
         await using var connection = context.RequestServices.GetService<MySqlConnection>();
-        await using var command = connection.CreateCommand();
 
-        command.CommandText = "SELECT * FROM user_cards_view WHERE username = @username";
-        command.Parameters.AddWithValue("@username", username);
+        if (connection != null) {
+            await connection.OpenAsync(); // open the connection
+            await using var command = connection.CreateCommand();
 
-        await using var reader = await command.ExecuteReaderAsync();
+            command.CommandText = "SELECT COUNT(*) FROM test.users WHERE username = @username";
+            command.Parameters.AddWithValue("@username", username);
 
-        while (await reader.ReadAsync())
-        {
-            string cardId = reader.GetString("cardid");
-            string src = reader.GetString("src");
-            await context.Response.WriteAsync($"<img id='{cardId}' src='{src}'></img>");
+            int userCount = Convert.ToInt32(await command.ExecuteScalarAsync());
+
+            if (userCount > 0) {
+                await context.Response.WriteAsync(await File.ReadAllTextAsync("wwwroot/html/home.html"));
+            } else {
+                context.Response.Redirect("/login");
+            }
+        } else {
+            Console.WriteLine($"[{DateTime.Now}] Connection from [HOME] is null");
         }
-    }
-    else
-    {
+    } else {
         context.Response.Redirect("/login");
     }
 });
@@ -89,12 +89,9 @@ app.MapMethods("/login", new[] { "GET", "POST" }, LoginHandler);
 
 app.MapMethods("/signup", new[] { "GET", "POST" }, async (context) =>
 {
-    if (context.Request.Method == "GET")
-    {
-        await context.Response.WriteAsync(await File.ReadAllTextAsync("wwwroot/html/signup.cshtml"));
-    }
-    else if (context.Request.Method == "POST")
-    {
+    if (context.Request.Method == "GET") {
+        await context.Response.WriteAsync(await File.ReadAllTextAsync("wwwroot/html/signup.html"));
+    } else if (context.Request.Method == "POST") {
         string? username = context.Request.Form["username"];
         string? email = context.Request.Form["email"];
         string? password = context.Request.Form["password"];
@@ -104,87 +101,80 @@ app.MapMethods("/signup", new[] { "GET", "POST" }, async (context) =>
         Console.WriteLine($"password: {password}");
 
         await using var connection = context.RequestServices.GetService<MySqlConnection>();
-        await connection.OpenAsync();
 
-        // Check if the username or email already exists
-        await using var checkCommand = connection.CreateCommand();
-        checkCommand.CommandText = "SELECT COUNT(*) FROM `users` WHERE username = @username OR email = @email";
-        checkCommand.Parameters.AddWithValue("@username", username);
-        checkCommand.Parameters.AddWithValue("@email", email);
-        long existingUserCount = (long)await checkCommand.ExecuteScalarAsync();
+        if (connection != null) {
+            await connection.OpenAsync();
 
-        if (existingUserCount > 0)
-        {
-            await context.Response.WriteAsync("Error: User already exists.");
-            return;
-        }
+            // Check if the username or email already exists
+            await using var checkCommand = connection.CreateCommand();
+            checkCommand.CommandText = "SELECT COUNT(*) FROM `users` WHERE username = @username OR email = @email";
+            checkCommand.Parameters.AddWithValue("@username", username);
+            checkCommand.Parameters.AddWithValue("@email", email);
 
-        // Insert the new user into the database
-        await using var insertCommand = connection.CreateCommand();
-        insertCommand.CommandText = "INSERT INTO `users` (username, email, pass) VALUES (@username, @email, @password)";
-        insertCommand.Parameters.AddWithValue("@username", username);
-        insertCommand.Parameters.AddWithValue("@email", email);
-        insertCommand.Parameters.AddWithValue("@password", password);
-        int rowsAffected = await insertCommand.ExecuteNonQueryAsync();
+            object? result = await checkCommand.ExecuteScalarAsync();
+            long existingUserCount = result != null ? (long)result : 0;
 
-        if (rowsAffected > 0)
-        {
-            // await context.Response.WriteAsync("User added successfully.");
+            if (existingUserCount > 0) {
+                await context.Response.WriteAsync("Error: User already exists.");
+                return;
+            }
+
+            // Insert the new user into the database
+            await using var insertCommand = connection.CreateCommand();
+            insertCommand.CommandText = "INSERT INTO `users` (username, email, pass) VALUES (@username, @email, @password)";
+            insertCommand.Parameters.AddWithValue("@username", username);
+            insertCommand.Parameters.AddWithValue("@email", email);
+            insertCommand.Parameters.AddWithValue("@password", password);
+            int rowsAffected = await insertCommand.ExecuteNonQueryAsync();
+        if (rowsAffected > 0) {
             context.Response.Redirect("/login");
-        }
-        else
-        {
+        } else {
             await context.Response.WriteAsync("Failed to add user.");
         }
-    }
-    else
-    {
+        } else {
+            Console.WriteLine($"[{DateTime.Now}] Connection from [SIGNUP] is null");
+        }
+    } else {
         context.Response.StatusCode = 405;
     }
 });
 
 async Task LoginHandler(HttpContext context)
 {
-    if (context.Request.Method == "GET")
-    {
+    if (context.Request.Method == "GET") {
         // Handle GET request, show the login form
-        await context.Response.WriteAsync(await File.ReadAllTextAsync("wwwroot/html/login.cshtml"));
-    }
-    else if (context.Request.Method == "POST")
-    {
+        await context.Response.WriteAsync(await File.ReadAllTextAsync("wwwroot/html/login.html"));
+    } else if (context.Request.Method == "POST") {
         // Handle POST request, validate login credentials and redirect
-        string username = context.Request.Form["username"];
-        string password = context.Request.Form["password"];
+        string? username = context.Request.Form["username"];
+        string? password = context.Request.Form["password"];
 
-        Console.WriteLine($"username: {username}");
-        Console.WriteLine($"password: {password}");
+        if (username != null && password != null) {
+            Console.WriteLine($"username: {username}");
+            Console.WriteLine($"password: {password}");
 
-        bool credentialsMatch = await ValidateCredentialsAsync(context, username, password);
+            bool credentialsMatch = await ValidateCredentialsAsync(context, username, password);
 
-        if (credentialsMatch)
-        {
-            // await context.Response.WriteAsync("login accepted");
-            context.Response.Cookies.Append("username", username); // Store the username in a cookie
-            context.Response.Redirect("/home");
+            if (credentialsMatch) {
+                // await context.Response.WriteAsync("login accepted");
+                context.Response.Cookies.Append("username", username); // Store the username in a cookie
+                context.Response.Redirect("/home");
+            } else {
+                context.Response.Redirect("/login?error=1");
+            }
+        } else {
+            Console.WriteLine($"[{DateTime.Now}] Username or password is null");
         }
-        else
-        {
-            context.Response.Redirect("/login?error=1");
-        }
-    }
-    else
-    {
+    } else {
         context.Response.StatusCode = 405; // Method Not Allowed
     }
 }
 
 async Task<bool> ValidateCredentialsAsync(HttpContext context, string username, string password)
 {
-    using (var connection = context.RequestServices.GetService<MySqlConnection>())
-    {
+    using (var connection = context.RequestServices.GetService<MySqlConnection>()) {
         await connection.OpenAsync();
-        using (var command = connection.CreateCommand())
-        {
+        using (var command = connection.CreateCommand()) {
             command.CommandText = "SELECT COUNT(*) FROM users WHERE username = @username AND pass = @password";
             command.Parameters.AddWithValue("@username", username);
             command.Parameters.AddWithValue("@password", password);
--- wwwroot/js/login_error.js
+++ wwwroot/js/login_error.js
@@ -1,27 +1,14 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     var urlParams = new URLSearchParams(window.location.search);
     var error = urlParams.get('error');
-
-    if (error !== null && error !== '') {
-        var errorMessage = document.getElementById('error-message');
-
+  
+    if (error) {
+        const errorMessage = document.getElementById('error-message');
+        errorMessage.style.display = 'block';
+        errorMessage.classList.add('move-down');
         setTimeout(() => {
-            errorMessage.classList.add('animate');
-        }, 1000); // 1000ms = 1 second delay
+            errorMessage.style.zIndex = '1';
+        }, 500); // Set the delay to 500 milliseconds (0.5 seconds)
     }
 });
-
-// Get the error message element
-const message = document.getElementById('error-message');
-
-// Add the 'animate' class to trigger the animation after a delay
-setTimeout(() => {
-    console.log("added class")
-    message.classList.add('animate');
-}, 1000); // 1000ms = 1 second delay
-
-
-
-
-
-Im trying to get the error message to work good luck
+  
